66637a879a227366a900ae591df094e69458b8b9
myanongithubplace <75791855+myanongithubplace@users.noreply.github.com>
2021-03-05 12:40:30 +0100

    Add files via upload

---
+++ src/assets/games.js
@@ -0,0 +1,552 @@
+export default {
+    sable: {
+      codes: ['000000'],
+      es: 'Sable',
+      en: 'Sable',
+      es_tip: 'Negro heráldico',
+      en_tip: 'Heraldic black',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://en.wikipedia.org/wiki/Sable_(heraldry)'
+    },
+    cordoban: {
+      codes: ['42352d'],
+      es: 'Cordobán',
+      en: 'Cordovan',
+      es_tip: 'Negro pardo cuero',
+      en_tip: 'Leather dark brown',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Cordob%C3%A1n_(color)'
+    },
+    umbra: {
+      codes: ['635147'],
+      es: 'Umbra',
+      en: 'Umber',
+      es_tip: 'Marrón rojizo',
+      en_tip: 'Reddish-brown ',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://en.wikipedia.org/wiki/Umber'
+    },
+    bistre: {
+      codes: ['967117'],
+      es: 'Bistre',
+      en: 'Bistre',
+      es_tip: 'Marrón amarillento',
+      en_tip: 'Yellowish brown',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://en.wikipedia.org/wiki/Bistre'
+    },
+    ocre:     {
+      codes: ['B9935A'],
+      es: 'Ocre',
+      en: 'Ochre',
+      es_tip: 'Marrón anaranjado o rojizo',
+      en_tip: 'Reddish brown',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Ocre'
+    },
+    grisPayne: {
+      codes: ['536878'],
+      es: 'Gris Payne',
+      en: "Payne's grey",
+      es_tip: 'Gris azulado',
+      en_tip: 'Bluish grey',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://encycolorpedia.es/536878'
+    },
+    grisPayneOscuro: {
+      codes: ['36454F'],
+      es: 'Payne oscuro',
+      en: "Dark Payne's",
+      es_tip: 'Gris oscuro azulado',
+      en_tip: 'Dark bluish grey',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Gris_de_Payne'
+    },
+    grisFeldgrau: {
+      codes: ['59646A'],
+      es: 'Feldgrau',
+      en: 'Feldgrau',
+      es_tip: 'Gris campaña',
+      en_tip: 'Field Grey',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Feldgrau'
+    },
+    turquí: {
+      codes: ['122562'],
+      es: 'Turquí',
+      en: 'Navy blue',
+      es_tip: 'Azul oscuro profundo',
+      en_tip: 'Dark shade of blue',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://encycolorpedia.es/122562'
+    },
+    añil: {
+      codes: ['264796'],
+      es: 'Añil',
+      en: 'Indigo',
+      es_tip: 'Azul oscuro y profundo. Índigo ó Glasto',
+      en_tip: 'Deep dark blue',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/A%C3%B1il'
+    },
+    indigo: {
+      codes: ['264796'],
+      es: 'Índigo',
+      en: 'Indigo',
+      es_tip: 'Azul oscuro y profundo. Añil ó Glasto',
+      en_tip: 'Deep dark blue',
+      available_in_es: true,
+      available_in_en: false,
+      source: 'https://es.wikipedia.org/wiki/A%C3%B1il'
+    },
+    glasto: {
+      codes: ['264796'],
+      es: 'Glasto',
+      en: 'Indigo',
+      es_tip: 'Azul oscuro y profundo. Índigo ó Añil',
+      en_tip: 'Deep dark blue',
+      available_in_es: true,
+      available_in_en: false,
+      source: 'https://es.wikipedia.org/wiki/A%C3%B1il'
+    },
+    cobalto: {
+      codes: ['0047AB'],
+      es: 'Cobalto',
+      en: 'Cobalt',
+      es_tip: 'Azul Cobalto',
+      en_tip: 'Cobalt blue',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://en.wikipedia.org/wiki/Cobalt_blue'
+    },
+    cobaltoClaro: {
+      codes: ['091F92'],
+      es: 'Cobalto Claro',
+      en: 'Light Cobalt',
+      es_tip: 'Azul Cobalto claro',
+      en_tip: 'Light Cobalt blue',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Azul_cobalto'
+    },
+    cobaltoOscuro: {
+      codes: ['004F79'],
+      es: 'Cobalto Oscuro',
+      en: 'Dark Cobalt',
+      es_tip: 'Azul Cobalto Oscuro',
+      en_tip: 'Dark Cobalt blue',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Azul_cobalto'
+    },
+    lapislazuli: {
+      codes: ['4273B8'],
+      es: 'Lapislázuli',
+      en: 'Lapis laculi',
+      es_tip: 'Azul marino templado',
+      en_tip: 'Mild navy blue',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Lapisl%C3%A1zuli'
+    },
+    cerúleoOscuro: {
+      codes: ['08457E'],
+      es: 'Cerúleo oscuro',
+      en: 'Dark Cerulean',
+      es_tip: 'Azul oscuro poco saturado',
+      en_tip: 'Dark less saturation blue',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Cer%C3%BAleo'
+    },
+    azur: {
+      codes: ['0066FF'],
+      es: 'Azur',
+      en: 'Azure',
+      es_tip: 'Azul heráldico',
+      en_tip: 'Heraldic blue',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Azur'
+    }, 
+    cerúleo: {
+      codes: ['0098D5'],
+      es: 'Cerúleo',
+      en: 'Cerulean',
+      es_tip: 'Azul medio poco saturado',
+      en_tip: 'Mild less saturation blue',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Cer%C3%BAleo'
+    },
+    cerceta: {
+      codes: ['009B94'],
+      es: 'Cerceta',
+      en: 'Teal',
+      es_tip: 'Azul verde',
+      en_tip: 'Blue-green',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Azul_verde'
+    },
+    verdeMalaquita: {
+      codes: ['12774f'],
+      es: 'Malaquita',
+      en: 'Malachite',
+      es_tip: 'Verde malaquita',
+      en_tip: 'Malachite green',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://mineralesdelmundo.com/malaquita/'
+    },
+    sinople: {
+      codes: ['008f4c'],
+      es: 'Sinople',
+      en: 'Sinople',
+      es_tip: 'Verde heráldico',
+      en_tip: 'Heraldic green',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://encycolorpedia.es/008f4c'
+    },
+    jade: {
+      codes: ['00a86b'],
+      es: 'Jade',
+      en: 'Jade',
+      es_tip: 'Verde jade',
+      en_tip: 'Jade green',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://jades.work/blog/color-jade/'
+    },
+    esmeralda: {
+      codes: ['009975'],
+      es: 'Esmeralda',
+      en: 'Emerald',
+      es_tip: 'Verde esmeralda',
+      en_tip: 'Emerald green',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://www.ecured.cu/Esmeralda_%28color%29'
+    },
+    viridian: {
+      codes: ['007F66'],
+      es: 'Viridián',
+      en: 'Viridian',
+      es_tip: 'Verde viridián',
+      en_tip: 'Viridián green',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://encycolorpedia.com/007f66'
+    },
+    veronese: {
+      codes: ['009D7A'],
+      es: 'Veronés',
+      en: 'Paolo Veronese',
+      es_tip: 'Verde Paolo Veronese',
+      en_tip: 'Paolo Veronese green',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Verde_Veronese'
+    },
+    cartujo: {
+      codes: ['BEB72D'],
+      es: 'Cartujo',
+      en: 'Chartreuse ',
+      es_tip: 'Verde cartujo',
+      en_tip: 'Chartreuse green',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Chartreuse_%28color%29'
+    },
+    gualda: {
+      codes: ['FABD00'],
+      es: 'Gualdo',
+      en: 'Goldtone',
+      es_tip: 'Amarillo heráldico (bandera de españa)',
+      en_tip: "Heraldic yellow (Spanish flag)",
+      available_in_es: true,
+      available_in_en: true,
+      source: 'http://www.jccanalda.es/jccanalda_doc/jccanalda_otrosarticulos/varia/gualda.htm'
+    },
+    hansa: {
+      codes: ['FFDD00'],
+      es: 'Hansa',
+      en: 'Arylide',
+      es_tip: 'Amarillo Hansa o monoazo',
+      en_tip: 'Arylide or Hansa yellow',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Amarillo_Hansa#Pintura_artística'
+    },
+    hansaMedio: {
+      codes: ['FFCD00'],
+      es: 'Hansa medio',
+      en: 'Medium arylide',
+      es_tip: 'Amarillo Hansa',
+      en_tip: 'Medium hansa yellow',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Amarillo_Hansa#Pintura_artística'
+    },
+    hansaOscuro: {
+      codes: ['FFB000'],
+      es: 'Hansa oscuro',
+      en: 'Dark arylide',
+      es_tip: 'Amarillo Hansa oscuro',
+      en_tip: 'Dark hansa yellow',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Amarillo_Hansa#Pintura_artística'
+    },
+    ambar: {
+      codes: ['E49E43'],
+      es: 'Ámbar',
+      en: 'Amber',
+      es_tip: 'Amarillo ámbar',
+      en_tip: 'Amber yellow',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/%C3%81mbar_%28color%29'
+    },
+    durazno: {
+      codes: ['FFCBA4'],
+      es: 'Durazno',
+      en: 'Peach',
+      es_tip: 'Color melocotón',
+      en_tip: 'Peach color',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Durazno_%28color%29#Variaciones_del_color_durazno'
+    },
+    ambrosía:       {
+      codes: ['f8eadd'],
+      es: 'Ambrosía',
+      en: 'Ambrosía',
+      es_tip: 'Color piel blanca',
+      en_tip: 'White skin color',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://encycolorpedia.es/f8eadd'
+    },
+    gamuza: {
+      codes: ['E8BD85'],
+      es: 'Gamuza',
+      en: 'Chamois',
+      es_tip: 'Naranja claro, saturación moderada',
+      en_tip: 'Light orange, light saturation',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Gamuza_%28color%29'
+    },
+    terracota: {
+      codes: ['C89C7D'],
+      es: 'Terracota',
+      en: 'Terra cotta',
+      es_tip: 'Naranja rojizo',
+      en_tip: 'Reddish orange',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://encycolorpedia.es/c89c7d'
+    },
+    ante: {
+      codes: ['BF8F4E'],
+      es: 'Ante',
+      en: 'Buff',
+      es_tip: 'Marrón textil',
+      en_tip: 'Textil brown',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Ante_%28color%29'
+    },
+    cobrizo: {
+      codes: ['CB6D51'],
+      es: 'Cobre',
+      en: 'Copper',
+      es_tip: 'Gama colores cobrizos',
+      en_tip: 'Range of "copperish" colours',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Cobre_(color)'
+    },
+    herrumbre: {
+      codes: ['A2522B'],
+      es: 'Herrumbre',
+      en: 'Rust',
+      es_tip: 'Hierro oxidado',
+      en_tip: 'Iron oxide, orange-red-brown',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Herrumbre_%28color%29'
+    },
+    siena: {
+      codes: ['882D17'],
+      es: 'Siena',
+      en: 'Sienna',
+      es_tip: 'Marrón rojizo pintura rupestre',
+      en_tip: 'Cave paintings reddish brown',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://en.wikipedia.org/wiki/Sienna'
+    },
+    borgoña: {
+      codes: ['673147'],
+      es: 'Borgoña',
+      en: 'Burgundy',
+      es_tip: 'Color del vino',
+      en_tip: 'Wine color',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Borgo%C3%B1a_%28color%29'
+    },
+    violin: {
+      codes: ['A10684'],
+      es: 'Violín',
+      en: 'Violine',
+      es_tip: 'Violeta purpureo',
+      en_tip: 'Purple violet',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://encycolorpedia.es/a10684'
+    },
+    bígaro:  {
+      codes: ['CCCCFF'],
+      es: 'Bígaro',
+      en: 'Periwinkle',
+      es_tip: 'Violeta azulado claro',
+      en_tip: 'Light bluish violet',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/B%C3%ADgaro_(color)'
+    },
+    malva: {
+      codes: ['E0B0FF'],
+      es: 'Malva',
+      en: 'Mauve',
+      es_tip: 'Purpura brillante',
+      en_tip: 'Brilliant purple',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Malva_(color)'
+    },
+    coral: {
+      codes: ['E4717A'],
+      es: 'Coral',
+      en: 'Coral',
+      es_tip: 'Rojo claro vivo',
+      en_tip: 'Alive light red',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Coral_%28color%29'
+    },
+    amaranto: {
+      codes: ['FF355E'],
+      es: 'Amaranto',
+      en: 'Amaranth',
+      es_tip: 'Rosa rojizo',
+      en_tip: 'Reddish pink',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Amaranto_%28color%29#cite_note-1'
+    },
+    rubí: {
+      codes: ['E0115F'],
+      es: 'Rubí',
+      en: 'Ruby',
+      es_tip: 'Rojo oscuro rosaceo',
+      en_tip: 'Pinkish dark red',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://encycolorpedia.com/e0115f'
+    },
+    escarlata: {
+      codes: ['ff0040'],
+      es: 'Escarlata',
+      en: 'Scarlet',
+      es_tip: 'Rojo brillante',
+      en_tip: 'Bright red',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://encycolorpedia.es/ff0040'
+    },
+    gules: {
+      codes: ['ed1c24'],
+      es: 'Gulés',
+      en: 'Gules',
+      es_tip: 'Rojo heráldico, intenso',
+      en_tip: 'Heraldic red, intense',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://encycolorpedia.es/ed1c24'
+    },
+    bermellon: {
+      codes: ['E62E00'],
+      es: 'Bermellón',
+      en: 'Vermilion',
+      es_tip: 'Rojo con toque naranja',
+      en_tip: 'Orange-coloured red',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Bermell%C3%B3n'
+    },
+    carmesí: {
+      codes: ['A51C30'],
+      es: 'Carmesí',
+      en: 'Crimson',
+      es_tip: 'Rojo fuerte purpureo ',
+      en_tip: 'Strong purplish red',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Carmes%C3%AD'
+    },
+    carmín: {
+      codes: ['960018'],
+      es: 'Carmin',
+      en: 'Carmine',
+      es_tip: 'Rojo oscuro levemente purpureo',
+      en_tip: 'Slightly purplish dark red',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Carm%C3%ADn'
+    },
+    caoba: {
+      codes: ['C04000'],
+      es: 'Caoba',
+      en: 'Mahogany ',
+      es_tip: 'Rojo madera',
+      en_tip: 'Wooden red',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://es.wikipedia.org/wiki/Caoba_%28color%29'
+    },
+    cardenal: {
+      codes: ['C41E3A'],
+      es: 'Cardenal',
+      en: 'Cardinal',
+      es_tip: 'Rojo vivo, toque rosa',
+      en_tip: 'Alive red, a touch of pink',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://encycolorpedia.es/c41e3a'
+    },
+    granate: {
+      codes: ['ab2a3e'],
+      es: 'Granate',
+      en: 'Maroon',
+      es_tip: 'Rojo oscuro de la granada',
+      en_tip: 'Dark red from pomegranate',
+      available_in_es: true,
+      available_in_en: true,
+      source: 'https://encycolorpedia.es/ab2a3e'
+    },
+}
